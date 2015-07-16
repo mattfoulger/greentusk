b5b3fbdbd4b637da8b19f1810d1a75c68992c4a7
mattfoulger <mfoulger@gmail.com>
2015-07-15 20:35:53 -0700

    initial commit, can retrieve notes and display contents

---
+++ Gemfile
@@ -0,0 +1,19 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'rake'
+gem 'activesupport'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'sinatra-activerecord'
+gem 'oauth'
+gem 'evernote_oauth'
+gem 'puma'
+gem 'tux'
+
+group :development, :test do
+  gem 'pry'
+  gem 'shotgun'
+  gem 'sqlite3'
+end
---
+++ evernote_config.rb
@@ -0,0 +1,15 @@
+# Load libraries required by the Evernote OAuth sample applications
+require 'oauth'
+require 'oauth/consumer'
+
+# Load Thrift & Evernote Ruby libraries
+require "evernote_oauth"
+
+# Client credentials
+# Fill these in with the consumer key and consumer secret that you obtained
+# from Evernote. If you do not have an Evernote API key, you may request one
+# from http://dev.evernote.com/documentation/cloud/
+OAUTH_CONSUMER_KEY = "elliotj112"
+OAUTH_CONSUMER_SECRET = "07d7756ec50451e0"
+
+SANDBOX = true
