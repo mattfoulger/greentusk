6d1951db4dc051c1163af3c9a55e856e1a3e0ff0
Matt Foulger <mfoulger@gmail.com>
2015-07-17 04:49:01 +0000

    showdown editor working

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,42 @@
 $(document).ready(function() {
+  var mdconverter = new showdown.Converter();
+  var editor = $("#editor");
+  var preview = $("#preview");
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function updatePreview(){
+    console.log("this should be updating");
+    preview.html(mdconverter.makeHtml(editor.val()));
+  }
+
+  updatePreview();
+
+  editor.on('keyup', function(){
+   updatePreview();
+  });
+
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    // Variables privadas
+    var links = this.el.find('.link');
+    // Evento
+    links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+  }
+
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el;
+      $this = $(this),
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+    };
+  } 
+
+  var accordion = new Accordion($('#accordion'), false);
+
 });
